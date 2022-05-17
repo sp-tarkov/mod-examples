8c7913f8967db35372b9930326449c5b9ebf4204
Chomp <chomp@noreply.dev.sp-tarkov.com>
2022-05-17 10:07:16 +0100

    Updates

--- TypeScript/4UseACustomConfigFile/src/mod.ts
+++ TypeScript/4UseACustomConfigFile/src/mod.ts
@@ -2,21 +2,22 @@ import type { DependencyContainer } from "tsyringe";
 
 import { IMod } from "../types/@types/external/mod";
 import type { ILogger } from "../types/@types/spt/utils/ILogger";
-import * as modConfig from "../config/config.json";
 
 class Mod implements IMod
 {
+    private modConfig = require("../config/config.json");
+
     // not used for this example
     public load(container: DependencyContainer): void
     { return }
 
     public delayedLoad(container: DependencyContainer): void
-    { 
+    {
         // get logger
         const logger = container.resolve<ILogger>("WinstonLogger");
 
         // log the 'myProperty' value to the console
-        logger.info(`here is the value from my config: ${modConfig.myProperty}`);
+        logger.info(`here is the value from my config: ${this.modConfig.myProperty}`);
     }
 }
 
