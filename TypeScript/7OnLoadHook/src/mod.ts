37ab32ea822bc8f831496b815990a949656e6487
Chomp <chomp@noreply.dev.sp-tarkov.com>
2022-05-23 22:11:50 +0100

    Update example 7

---
+++ TypeScript/7OnLoadHook/src/mod.ts
@@ -0,0 +1,30 @@
+import { DependencyContainer } from "tsyringe";
+import { IMod } from "../types/models/external/mod"
+import { ILogger } from "../types/models/spt/utils/ILogger"
+import { OnLoadModService } from "../types/services/mod/onLoad/OnLoadModService"
+
+class Mod implements IMod
+{
+    public load(container: DependencyContainer): void
+    {
+        const logger = container.resolve<ILogger>("WinstonLogger");
+        const onLoadModService = container.resolve<OnLoadModService>("OnLoadModService");
+        onLoadModService.registerOnLoad(
+            "MyCustomMod", 
+            () => this.customFunctionThatRunsOnLoad(logger), 
+            () => "custom-mod" // new route name
+        )
+    }
+
+    customFunctionThatRunsOnLoad(logger: ILogger): void
+    {
+        logger.info("MyCustomMod custom function is loading right now")
+    }
+
+    // Not used in this example
+    public delayedLoad(container: DependencyContainer): void
+    {
+        return;
+    }
+}
+module.exports = {mod: new Mod()}
---
+++ TypeScript/7OnLoadHook/types/utils/Watermark.d.ts
@@ -0,0 +1,28 @@
+import { ConfigServer } from "../servers/ConfigServer";
+import { ILogger } from "../models/spt/utils/ILogger";
+export declare class WatermarkLocale {
+    private locales;
+    getLocale(): string;
+    getDescription(): string[];
+    getWarning(): string[];
+}
+export declare class Watermark {
+    private logger;
+    private configServer;
+    private watermarkLocale?;
+    private akiConfig;
+    constructor(logger: ILogger, configServer: ConfigServer, watermarkLocale?: WatermarkLocale);
+    private text;
+    private versionLabel;
+    initialize(): void;
+    getVersionTag(): string;
+    getVersionLabel(): string;
+    /** Set window title */
+    setTitle(): void;
+    /** Reset console cursor to top */
+    resetCursor(): void;
+    /** Draw the watermark */
+    draw(): void;
+    /** Caculate text length */
+    private textLength;
+}
