cc73e0591c9b10b8af1a520b00c49ca81a9f0b08
Chomp <chomp@noreply.dev.sp-tarkov.com>
2022-06-21 11:07:17 +0100

    Remove duplicate

--- TypeScript/11AddTrader/types/controllers/HandbookController.d.ts
+++
@@ -1,8 +0,0 @@
-import { DatabaseServer } from "../servers/DatabaseServer";
-import { HandbookHelper } from "../helpers/HandbookHelper";
-export declare class HandbookController {
-    private databaseServer;
-    private handbookHelper;
-    constructor(databaseServer: DatabaseServer, handbookHelper: HandbookHelper);
-    load(): void;
-}
--- TypeScript/11AddTrader/types/services/RagfairLinkedItemService.d.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseServer } from "../servers/DatabaseServer";
-export declare class RagfairLinkedItemService {
-    private databaseServer;
-    private linkedItemsCache;
-    constructor(databaseServer: DatabaseServer);
-    getLinkedItems(linkedSearchId: string): Iterable<string>;
-    buildLinkedItemTable(): void;
-    private getFilters;
-}
