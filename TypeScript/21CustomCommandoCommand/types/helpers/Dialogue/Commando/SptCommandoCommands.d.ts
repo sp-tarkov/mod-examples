393f9d4fc432135b526c9e089fb4994e3049d2cb
Dev <dev@dev.sp-tarkov.com>
2024-02-25 14:17:49 +0000

    Update types

--- TypeScript/21CustomCommandoCommand/types/helpers/Dialogue/Commando/SptCommandoCommands.d.ts
+++ TypeScript/21CustomCommandoCommand/types/helpers/Dialogue/Commando/SptCommandoCommands.d.ts
@@ -1,9 +1,9 @@
-import { IChatCommand } from "@spt-aki/helpers/Dialogue/Commando/IChatCommand";
+import { ICommandoCommand } from "@spt-aki/helpers/Dialogue/Commando/ICommandoCommand";
 import { ISptCommand } from "@spt-aki/helpers/Dialogue/Commando/SptCommands/ISptCommand";
 import { ISendMessageRequest } from "@spt-aki/models/eft/dialog/ISendMessageRequest";
 import { IUserDialogInfo } from "@spt-aki/models/eft/profile/IAkiProfile";
 import { ConfigServer } from "@spt-aki/servers/ConfigServer";
-export declare class SptCommandoCommands implements IChatCommand {
+export declare class SptCommandoCommands implements ICommandoCommand {
     protected configServer: ConfigServer;
     protected sptCommands: ISptCommand[];
     constructor(configServer: ConfigServer, sptCommands: ISptCommand[]);
--- TypeScript/22CustomAkiCommand/types/helpers/Dialogue/CommandoDialogueChatBot.d.ts
+++ TypeScript/22CustomAkiCommand/types/helpers/Dialogue/CommandoDialogueChatBot.d.ts
@@ -1,10 +1,15 @@
-import { IChatCommand } from "@spt-aki/helpers/Dialogue/Commando/IChatCommand";
+import { ICommandoCommand } from "@spt-aki/helpers/Dialogue/Commando/ICommandoCommand";
+import { IDialogueChatBot } from "@spt-aki/helpers/Dialogue/IDialogueChatBot";
+import { ISendMessageRequest } from "@spt-aki/models/eft/dialog/ISendMessageRequest";
 import { IUserDialogInfo } from "@spt-aki/models/eft/profile/IAkiProfile";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { MailSendService } from "@spt-aki/services/MailSendService";
-import { AbstractDialogueChatBot } from "@spt-aki/helpers/Dialogue/AbstractDialogueChatBot";
-export declare class CommandoDialogueChatBot extends AbstractDialogueChatBot {
-    constructor(logger: ILogger, mailSendService: MailSendService, chatCommands: IChatCommand[]);
+export declare class CommandoDialogueChatBot implements IDialogueChatBot {
+    protected logger: ILogger;
+    protected mailSendService: MailSendService;
+    protected commandoCommands: ICommandoCommand[];
+    constructor(logger: ILogger, mailSendService: MailSendService, commandoCommands: ICommandoCommand[]);
+    registerCommandoCommand(commandoCommand: ICommandoCommand): void;
     getChatBot(): IUserDialogInfo;
-    protected getUnrecognizedCommandMessage(): string;
+    handleMessage(sessionId: string, request: ISendMessageRequest): string;
 }
