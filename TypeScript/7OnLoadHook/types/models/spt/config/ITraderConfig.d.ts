b992eaf33e7beb2cb6ca8d72db5b653a2ff3c193
Dev <dev@dev.sp-tarkov.com>
2024-03-31 16:47:39 +0100

    Update types

--- TypeScript/7OnLoadHook/types/models/spt/config/ITraderConfig.d.ts
+++ TypeScript/7OnLoadHook/types/models/spt/config/ITraderConfig.d.ts
@@ -9,8 +9,6 @@ export interface ITraderConfig extends IBaseConfig {
     tradersResetFromServerStart: boolean;
     updateTimeDefault: number;
     traderPriceMultipler: number;
-    /** Keep track of purchased trader-limited items beyond server restarts to prevent server-restart item scumming */
-    persistPurchaseDataInProfile: boolean;
     fence: FenceConfig;
 }
 export interface UpdateTime {
