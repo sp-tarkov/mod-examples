851e20d35dc196cd0f92bbbc46f4e8fea1bd73cc
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-30 11:41:56 -0700

    Manually pull in example 16 change from 3.11.0-dev branch

--- TypeScript/16ImporterUtil/src/mod.ts
+++ TypeScript/16ImporterUtil/src/mod.ts
@@ -1,13 +1,13 @@
 import { DependencyContainer } from "tsyringe";
 
 import { PreSptModLoader } from "@spt/loaders/PreSptModLoader";
-import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { ImporterUtil } from "@spt/utils/ImporterUtil";
 import { ConfigsModelBase } from "./model/ConfigsModel";
+import { IPreSptLoadModAsync } from "@spt/models/external/IPreSptLoadModAsync";
 
-class Mod implements IPreSptLoadMod {
-    public preSptLoad(container: DependencyContainer): void {
+class Mod implements IPreSptLoadModAsync {
+    public async preSptLoadAsync(container: DependencyContainer): Promise<void> {
         // get logger
         const logger = container.resolve<ILogger>("WinstonLogger");
 
@@ -16,7 +16,7 @@ class Mod implements IPreSptLoadMod {
         const path = modImporter.getModPath("16ImporterUtil");
 
         const configPath = `${path}config/`;
-        const configs = importerUtil.loadRecursive<ConfigsModelBase>(configPath);
+        const configs = await importerUtil.loadAsync<ConfigsModelBase>(configPath);
         logger.info(
             `16ImporterUtil Configurations found: ${JSON.stringify(configs)}`,
         );
